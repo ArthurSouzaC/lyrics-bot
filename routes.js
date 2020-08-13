31fc90ea103d25c7cc116ab5733b2ba019a76731
ArthurSouzaC <arthursouza.info@gmail.com>
2020-08-13 17:25:29 -0300

    adding url

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const routes = new express.Router()
 
 routes.get('/', (req, res) => {
     res.json({
-        "message": "homepage"
+        "url": "https://discord.com/api/oauth2/authorize?client_id=743183220308443298&permissions=36825088&scope=bot"
     })
 })
 
